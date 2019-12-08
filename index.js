ff01f9513fc50dd4f153baa4ce92950a9e646cbc
Keelana Char <keelanachar@gmail.com>
2019-12-08 11:49:53 -0600

    add mathjs and babel

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 /* global moment */
+/* global evaluate */
+
+var moment = require('moment');
+var math = require('mathjs');
 
 console.log('js here');
 
 console.log(moment().format("MMM Do YY"));
+console.log(math.log(10000, 10));
+
+var color = "green";
+console.log(`the car is ${color}`);
